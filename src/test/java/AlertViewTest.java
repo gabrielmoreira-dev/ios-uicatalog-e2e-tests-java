e8228f924fc4a5ce596af0cc9251acb5167b83ff
Gabriel Moreira <g.alves.m.2008@gmail.com>
2024-12-25 03:06:09 -0300

    Added alert view test

--- src/test/java/AlertViewTest.java
+++ src/test/java/AlertViewTest.java
@@ -1,9 +1,23 @@
 import io.appium.java_client.AppiumBy;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AlertViewTest extends IOSBaseTest {
     @Test
-    public void testIOSApp() {
+     void testClickOnAlertView() {
         driver.findElement(AppiumBy.accessibilityId("Alert Views")).click();
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeStaticText[`name == \"Text Entry\"`]")).click();
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeTextField")).sendKeys("Hello World");
+        driver.findElement(AppiumBy.accessibilityId("OK")).click();
+    }
+
+    @Test
+    void testClickOnClickCancelView() {
+        driver.findElement(AppiumBy.accessibilityId("Alert Views")).click();
+        driver.findElement(AppiumBy.accessibilityId("Confirm / Cancel")).click();
+
+        String text = driver.findElement(AppiumBy.iOSNsPredicateString("name BEGINSWITH[c] 'A message'")).getText();
+
+        Assert.assertEquals(text, "A message should be a short, complete sentence.");
     }
 }
